a9cb9d709c0c34b56bc6495129fc0fb757b64cb1
Viacheslau <v_sadovsky@mail.ru>
2020-10-08 23:14:58 +0300

    Led added
    Try to control led via photo resistor

--- lesson_03_photo_sensor_simple/lesson_03_photo_sensor_simple.ino
+++ lesson_03_photo_sensor_simple/lesson_03_photo_sensor_simple.ino
@@ -1,8 +1,10 @@
 #define PHOTO_PIN A1
+#define LED_PIN   9
+#define DAY_LIGHT 10
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(LED_PIN, OUTPUT);
 }
 
 int photo_signal;
@@ -12,4 +14,10 @@ void loop() {
   Serial.print("ADC conversion value is ");
   Serial.println(photo_signal); 
   delay(500);
+
+  if (photo_signal < DAY_LIGHT) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
